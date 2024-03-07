e13a2858554d9f1acd976189bda04a5fcf1dc633
Sergey Shershikov <ssher250110@yandex.ru>
2024-03-07 22:07:17 +0300

    main structure app

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 
+from catalog.apps import CatalogConfig
+from catalog.views import home_page, contact_info
+
+app_name = CatalogConfig.name
 urlpatterns = [
-    path()
+    path('', home_page, name='home_page'),
+    path('contact/', contact_info, name='contact_info')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def home_page(request):
+    return render(request, 'home_page.html')
+
+
+def contact_info(request):
+    return render(request, 'contact_info.html')
