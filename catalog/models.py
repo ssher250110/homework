908473bfdd436f649efabccf5807492418d89142
Sergey Shershikov <ssher250110@yandex.ru>
2024-03-16 02:10:40 +0300

    added srtucture models.py

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    pass
+
+    def __str__(self):
+        pass
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+class Product(models.Model):
+    pass
+
+    def __str__(self):
+        pass
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
