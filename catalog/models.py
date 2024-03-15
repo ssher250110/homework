02176b5f70a5d41d800876463825452fd719af82
Sergey Shershikov <ssher250110@yandex.ru>
2024-03-16 02:32:11 +0300

    filled models

--- catalog/models.py
+++ catalog/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
+NULLABLE = {'null': True, 'blank': True}
+
 
 # Create your models here.
 class Category(models.Model):
-    pass
+    name = models.CharField(max_length=100, verbose_name='Категория продукта')
+    description = models.TextField(**NULLABLE, verbose_name='Описание')
 
     def __str__(self):
-        pass
+        return f'{self.name}'
 
     class Meta:
         verbose_name = 'категория'
@@ -14,10 +17,16 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    pass
+    name = models.CharField(max_length=100, verbose_name='Продукт')
+    description = models.TextField(**NULLABLE, verbose_name='Описание')
+    photo = models.ImageField(upload_to='products/', **NULLABLE, verbose_name='Изображение')
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, verbose_name='Категория продукта')
+    price = models.IntegerField(verbose_name='Цена')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
 
     def __str__(self):
-        pass
+        return f'{self.name}: {self.price} ({self.category})'
 
     class Meta:
         verbose_name = 'продукт'
