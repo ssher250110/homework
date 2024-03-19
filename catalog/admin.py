36212e09e0487568a7d4a0851cdc582489af0f35
Sergey Shershikov <ssher250110@yandex.ru>
2024-03-19 21:34:39 +0300

    added filter and search

--- catalog/admin.py
+++ catalog/admin.py
@@ -11,3 +11,5 @@ class CategoryAdmin(admin.ModelAdmin):
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('pk', 'name', 'price', 'category',)
+    list_filter = ('category',)
+    search_fields = ('name', 'description',)
