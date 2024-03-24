17c05a6d7b3e214c26b9e9786135116f09ecff0a
Sergey Shershikov <ssher250110@yandex.ru>
2024-03-24 20:48:24 +0300

    removal old html hw 2, adding base.html and view.py

--- catalog/views.py
+++ catalog/views.py
@@ -2,14 +2,5 @@ from django.shortcuts import render
 
 
 # Create your views here.
-def home_page(request):
-    return render(request, 'home_page.html')
-
-
-def contact_info(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        message = request.POST.get('message')
-        print(f"Имя: {name}, Номер телефона: {phone}, Сообщение: {message}")
-    return render(request, 'contact_info.html')
+def base(request):
+    return render(request, 'base.html')
