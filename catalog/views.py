01c0ddae4a29817f200cb75b1f9b9e331d6afe65
Sergey Shershikov <ssher250110@yandex.ru>
2024-03-25 01:31:31 +0300

    adding title page

--- catalog/views.py
+++ catalog/views.py
@@ -7,6 +7,7 @@ def product_info(request, pk):
     product = Product.objects.get(pk=pk)
     context = {
         'product': product,
+        'title': 'Page product',
     }
     return render(request, 'catalog/product_info.html', context)
 
@@ -15,5 +16,7 @@ def index(request):
     product_list = Product.objects.all()
     context = {
         'products': product_list,
+        'title': 'Main page',
+        'description': 'Auto parts'
     }
     return render(request, 'catalog/index.html', context)
