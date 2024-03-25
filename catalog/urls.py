94c95cb786c6a34faad2ecdcfa189720ddad4fbe
Sergey Shershikov <ssher250110@yandex.ru>
2024-03-25 04:55:03 +0300

    adding contact

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import product_info, index
+from catalog.views import product_info, index, contact_info
 
 app_name = CatalogConfig.name
 urlpatterns = [
     path('catalog/<int:pk>', product_info, name='product_info'),
     path('', index, name='index'),
+    path('contact', contact_info, name='contact_info'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -20,3 +20,12 @@ def index(request):
         'description': 'Auto parts'
     }
     return render(request, 'catalog/index.html', context)
+
+
+def contact_info(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f"Имя: {name}, Номер телефона: {phone}, Сообщение: {message}")
+    return render(request, 'catalog/contact_info.html')
