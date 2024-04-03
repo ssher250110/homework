a8c07af44bc2307b401886f111fc42dd1c1f9b34
Sergey Shershikov <ssher250110@yandex.ru>
2024-04-04 00:10:49 +0300

    added /

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,7 +6,7 @@ from catalog.views import contact_info, ProductListView, ProductDetailView, Prod
 app_name = CatalogConfig.name
 urlpatterns = [
     path('', ProductListView.as_view(), name='index'),
-    path('catalog/<int:pk>', ProductDetailView.as_view(), name='product_info'),
-    path('catalog/create', ProductCreateView.as_view(), name='create_product'),
+    path('catalog/<int:pk>/', ProductDetailView.as_view(), name='product_info'),
+    path('catalog/create/', ProductCreateView.as_view(), name='create_product'),
     path('contact', contact_info, name='contact_info'),
 ]
