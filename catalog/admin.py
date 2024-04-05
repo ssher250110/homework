aefb301ed2ac13932afce20470eb9e09f72a6a8e
Sergey Shershikov <ssher250110@yandex.ru>
2024-04-05 18:14:11 +0300

    added view blog admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from catalog.models import Category, Product
+from catalog.models import Category, Product, Blog
 
 
 @admin.register(Category)
@@ -13,3 +13,8 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('pk', 'name', 'price', 'category',)
     list_filter = ('category',)
     search_fields = ('name', 'description',)
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'status', 'view_count', 'product')
