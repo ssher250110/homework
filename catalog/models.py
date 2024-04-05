b948f47722b578d0871dfd63cf2878d625800a3f
Sergey Shershikov <ssher250110@yandex.ru>
2024-04-05 22:20:55 +0300

    editing model product

--- catalog/models.py
+++ catalog/models.py
@@ -52,7 +52,7 @@ class Blog(models.Model):
     class Meta:
         verbose_name = 'Блог'
         verbose_name_plural = 'Блоги'
-        ordering = ['-created_at']
+        ordering = ['-created_at', ]
 
     def __str__(self):
         return self.title
