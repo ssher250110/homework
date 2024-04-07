2d0fc2f5ded091c6ca0da50fa511dcc818f05306
Sergey Shershikov <ssher250110@yandex.ru>
2024-04-07 22:08:30 +0300

    added view_count

--- catalog/views.py
+++ catalog/views.py
@@ -84,6 +84,12 @@ class BlogDetailView(DetailView):
         context['title'] = 'Page blog'
         return context
 
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.view_count += 1
+        self.object.save()
+        return self.object
+
 
 class BlogCreateView(CreateView):
     model = Blog
