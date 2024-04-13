4e17490d756403e075d2e7cea1e3c37657ef7aef
Sergey Shershikov <ssher250110@yandex.ru>
2024-04-13 17:31:01 +0300

    editing clean method

--- catalog/forms.py
+++ catalog/forms.py
@@ -16,11 +16,12 @@ class ProductForm(forms.ModelForm):
         cleaned_data = self.cleaned_data['name']
         for data_word in exclude_words:
             if re.search(data_word, cleaned_data.lower(), flags=re.S) is not None:
-                raise forms.ValidationError('Запрещена загрузка товара')
+                raise forms.ValidationError(f'Название товара содержит запрещенное слово - {data_word}')
         return cleaned_data
 
     def clean_description(self):
         cleaned_data = self.cleaned_data['description']
         for data_word in exclude_words:
             if re.search(data_word, cleaned_data.lower(), flags=re.S) is not None:
-                raise forms.ValidationError('Запрещено добавлять слово')
+                raise forms.ValidationError(f'Описание товара содержит запрещенное слово - {data_word}')
+        return cleaned_data
