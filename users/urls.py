ea4ee6121da545025a442b0545bf4a84de37d18c
Sergey Shershikov <ssher250110@yandex.ru>
2024-04-19 16:37:29 +0300

    added login and logout

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from users.apps import UsersConfig
@@ -7,4 +7,5 @@ app_name = UsersConfig.name
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
