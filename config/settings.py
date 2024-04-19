d219b800524983e44b7b45f2dac94025e4e6dd83
Sergey Shershikov <ssher250110@yandex.ru>
2024-04-19 18:48:47 +0300

    added user database environment variables

--- config/settings.py
+++ config/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'hw_django',
-        'USER': 'ssher'
+        'USER': os.getenv('USER_DATABASE')
     }
 }
 
