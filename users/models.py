a47475dffd05f57d57c1c8546c85780fae2aa095
Sergey Shershikov <ssher250110@yandex.ru>
2024-04-19 14:58:31 +0300

    added model user

--- users/models.py
+++ users/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from catalog.models import NULLABLE
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='Почта', help_text='Введите адрес электронной почты')
+    phone = models.CharField(max_length=35, **NULLABLE, verbose_name='Телефон', help_text='Введите номер телефона')
+    avatar = models.ImageField(upload_to='users/avatars', **NULLABLE, verbose_name='Аватар',
+                               help_text='Загрузите свой аватар')
+    country = models.CharField(max_length=50, **NULLABLE, verbose_name='Страна', help_text='Введите название страны')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
