36f9636638a589f8f9d38051af739b3380f5df94
Sergey Shershikov <ssher250110@yandex.ru>
2024-04-19 19:33:52 +0300

    added Sending emails for yandex.ru

--- users/views.py
+++ users/views.py
@@ -1,6 +1,11 @@
-from django.urls import reverse_lazy
+import secrets
+
+from django.core.mail import send_mail
+from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView
 
+from config.settings import EMAIL_HOST_USER
 from users.forms import UserRegisterForm
 from users.models import User
 
@@ -9,3 +14,26 @@ class UserCreateView(CreateView):
     model = User
     form_class = UserRegisterForm
     success_url = reverse_lazy('users:login')
+
+    def form_valid(self, form):
+        user = form.save()
+        user.is_active = False
+        token = secrets.token_hex(16)
+        user.token = token
+        user.save()
+        host = self.request.get_host()
+        url = f'http://{host}/users/email-confirm/{token}/'
+        send_mail(
+            subject='Подтверждение почты',
+            message=f'Перейти по ссылке для подтверждения почты {url}',
+            from_email=EMAIL_HOST_USER,
+            recipient_list=[user.email],
+        )
+        return super().form_valid(form)
+
+    @staticmethod
+    def email_verification(request, token):
+        user = get_object_or_404(User, token=token)
+        user.is_active = True
+        user.save()
+        return redirect(reverse('users:login'))
